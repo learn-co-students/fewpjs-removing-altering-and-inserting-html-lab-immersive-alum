d09fc6287935cddabfb4517664d86245ea1e6915
thuyanduong <thuyanannduong@gmail.com>
2019-04-10 19:04:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("#main").remove()
+
+
+var newHeader = document.createElement("h1")
+debugger
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Ann is the champion'
